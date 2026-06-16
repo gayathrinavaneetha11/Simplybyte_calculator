be5867ee273a33b6d73ab610feeac0ed48f6bca3
gayathrinavaneetha11 <gayathrinavaneethapl1169.sse@saveetha.com>
2026-06-16 13:30:51 +0530

    changed

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 5000;
+const PORT = 7000;
+const HOST = '0.0.0.0'; // Tells the server to accept traffic from outside the container
 
 http.createServer((req, res) => {
   const filePath = path.join(__dirname, 'calculator.html');
@@ -16,6 +17,6 @@ http.createServer((req, res) => {
       res.end(content, 'utf-8');
     }
   });
-}).listen(PORT, () => {
+}).listen(PORT, HOST, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
